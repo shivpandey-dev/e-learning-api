53a2db4e1917412ef6115c74c1e1c679dffe3ce9
Shiv Pandey <pandey.shiv09@gmail.com>
2025-08-15 02:57:50 +0530

    chore: make global prefix dynamic for dev, stage, prod

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  app.setGlobalPrefix('api/prod');
+  const env = process.env.NODE_ENV || 'development';
+  let prefix = 'api/dev';
+  if (env === 'production') prefix = 'api/prod';
+  else if (env === 'staging') prefix = 'api/stage';
+  app.setGlobalPrefix(prefix);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   await app.listen(Number(process.env.PORT) || 4300);
 }
